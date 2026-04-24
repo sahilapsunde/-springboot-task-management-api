52ebcb8578d5d0eb5f02a93997c32e1fadd70415
sahilapsunde <sahil.apsunde51@gmail.com>
2026-04-24 11:10:42 +0530

    Create TaskManagementApiApplication.java

---
+++ src/main/java/com/sahil/taskapi/TaskManagementApiApplication.java
@@ -0,0 +1,9 @@
+package com.sahil.taskapi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class TaskManagementApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementApiApplication.class, args);
+    }
+}
